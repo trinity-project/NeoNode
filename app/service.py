1364b40b40ce18a4504e6785a0e7c7ad0c016c01
maiganne <1730105653@qq.com>
2018-06-28 10:39:22 +0800

    modify

--- app/service.py
+++ app/service.py
@@ -133,7 +133,7 @@ def get_transaction_by_address(address,asset,timestamp=None,page=1):
 
         query_tx = InvokeTx.query.filter(
             or_(InvokeTx.address_from == address, InvokeTx.address_to == address),
-            timestamp_filter
+            # timestamp_filter
         ).paginate(page=page,per_page=2).items
 
 
