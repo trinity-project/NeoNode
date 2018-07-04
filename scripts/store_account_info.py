1a1a63b55506124b28bd34718f89c2f7c7dffe44
maiganne <1730105653@qq.com>
2018-07-04 19:37:40 +0800

    modify

--- scripts/store_account_info.py
+++ scripts/store_account_info.py
@@ -86,8 +86,9 @@ while True:
         continue
     try:
         redis_client.publish("monitor", json.dumps({"playload": block_h, "messageType": "monitorBlockHeight"}))
-    except:
+    except Exception as e:
         logger.error("connect redis fail")
+        logger.error(e)
     if local_block_count<=block_h.height-1:
         exist_instance=Tx.query(local_block_count)
         if  exist_instance:
