be084e6ca91fb5e86e8c2f06351ab7c2e520e8fd
maiganne <1730105653@qq.com>
2018-09-03 20:01:38 +0800

    modify

--- app/service.py
+++ app/service.py
@@ -97,7 +97,7 @@ def get_balance(address,assetId):
         response={
             "gasBalance":gas_balance,
             "neoBalance":neo_balance,
-            "tncBalance":int(hex_reverse(value),16)/100000000 if value else 0
+            "tncBalance":Decimal(int(hex_reverse(value), 16)) / (10**8) if value else 0
         }
 
         return response
@@ -110,7 +110,7 @@ def get_balance(address,assetId):
         else:
             try:
                 res = _get_nep5_balance(address,assetId)
-                value = int(hex_reverse(res), 16) / 100000000 if res else 0,
+                value = Decimal(int(hex_reverse(res), 16)) / (10**8) if res else 0,
                 print(value)
                 return value
             except:
