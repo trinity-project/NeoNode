f923127bd4fce581508968140edb3889b3520532
maiganne <1730105653@qq.com>
2018-09-14 13:50:06 +0800

    modify

--- runserver.py
+++ runserver.py
@@ -1,6 +1,16 @@
+import os
 from app import app
 
+ENVIRON=os.environ
 
+
+if ENVIRON.get("CURRENT_ENVIRON") == "mainnet":
+    host = "127.0.0.1"
+    debug = False
+else:
+
+    host = "0.0.0.0"
+    debug = True
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=21332, debug=True)
+    app.run(host=host, port=21332, debug=debug)
