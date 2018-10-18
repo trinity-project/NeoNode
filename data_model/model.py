56b4cfa8b4b18bca852bcd2c25e0a68a4761d72a
maiganne <1730105653@qq.com>
2018-10-18 19:22:27 +0800

    添加存储nep5合约脚本

---
+++ data_model/model.py
@@ -0,0 +1,143 @@
+import pymysql
+from sqlalchemy.dialects.mysql import LONGTEXT
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, create_engine, SmallInteger, DECIMAL, Index, Boolean, or_, UniqueConstraint, \
+    String, Text
+from sqlalchemy.orm import sessionmaker
+
+from config import setting
+from project_log import setup_mylogger
+
+logger=setup_mylogger(logfile="log/store_account_info.log")
+
+
+
+
+pymysql.install_as_MySQLdb()
+
+
+
+
+block_info_engine = create_engine('mysql://%s:%s@%s/%s' %(setting.MYSQLDATABASE["user"],
+                                               setting.MYSQLDATABASE["passwd"],
+                                               setting.MYSQLDATABASE["host"],
+                                               setting.MYSQLDATABASE["db_block_info"]),
+                                  pool_recycle=3600,pool_size=100)
+
+account_info_engine = create_engine('mysql://%s:%s@%s/%s' %(setting.MYSQLDATABASE["user"],
+                                               setting.MYSQLDATABASE["passwd"],
+                                               setting.MYSQLDATABASE["host"],
+                                               setting.MYSQLDATABASE["db_account_info"]
+                                                            ),
+                                   pool_recycle=3600,pool_size=100)
+
+
+
+BlockInfoSession = sessionmaker(bind=block_info_engine)
+AccountInfoSession = sessionmaker(bind=account_info_engine)
+
+BlockInfoBase = declarative_base()
+AccountInfoBase = declarative_base()
+
+
+
+class LocalBlockCout(AccountInfoBase):
+    __tablename__ = 'bookmark_for_token'
+    id = Column(Integer, primary_key=True)
+    height = Column(Integer)
+
+    @staticmethod
+    def query():
+        session=AccountInfoSession()
+        exist_instance=session.query(LocalBlockCout).first()
+        session.close()
+        return exist_instance
+    @staticmethod
+    def save(height):
+        session=AccountInfoSession()
+        new_instance = LocalBlockCout(height=height)
+        session.add(new_instance)
+        try:
+            session.commit()
+        except:
+            session.rollback()
+        finally:
+            session.close()
+        return new_instance
+    @staticmethod
+    def update(exist_instance):
+        session=AccountInfoSession()
+        session.add(exist_instance)
+        try:
+            session.commit()
+        except:
+            session.rollback()
+        finally:
+            session.close()
+
+class BlockHeight(BlockInfoBase):
+    __tablename__ = 'local_block_count'
+    id = Column(Integer, primary_key=True)
+    height = Column(Integer)
+
+    @staticmethod
+    def query():
+        session=BlockInfoSession()
+        exist_instance=session.query(BlockHeight).first()
+        session.close()
+        return exist_instance
+
+
+class Token(AccountInfoBase):
+    __tablename__ = 'token'
+    id = Column(Integer, primary_key=True)
+    address = Column(String(42),unique=True)
+    name = Column(String(32),index=True)
+    symbol = Column(String(8),index=True)
+    decimal = Column(String(2))
+    token_type = Column(String(8))
+    chain_type = Column(String(8))
+    icon = Column(String(256))
+
+    @staticmethod
+    def query_token(address):
+        session = AccountInfoSession()
+        exist_instance = session.query(LocalBlockCout).filter(Token.address==address).first()
+        return exist_instance
+
+
+    @staticmethod
+    def save(address,name,symbol,decimal,tokenType,chainType,icon):
+        session = AccountInfoSession()
+        new_instance = Token(address=address, name=name,symbol=symbol,decimal=decimal,
+                             token_type = tokenType,chain_type=chainType,icon=icon)
+        session.add(new_instance)
+        try:
+            session.commit()
+            return True
+        except Exception as e:
+            return False
+
+
+
+
+
+class Tx(BlockInfoBase):
+    __tablename__ = 'tx'
+    id = Column(Integer, primary_key=True)
+    tx_id = Column(String(66),unique=True)
+    tx_type = Column(String(32))
+    block_height=Column(Integer,index=True)
+    block_time=Column(Integer)
+    vin = Column(LONGTEXT)
+    vout = Column(LONGTEXT)
+    script=Column(Text)
+
+
+    @staticmethod
+    def query(block_height):
+        session=BlockInfoSession()
+        exist_instance=session.query(Tx).filter(Tx.block_height==block_height).all()
+        session.close()
+        return exist_instance
+
---
+++ store_token.py
@@ -0,0 +1,124 @@
+import json
+import random
+import time
+import binascii
+import requests
+from decimal import Decimal
+from neocore.Cryptography.Crypto import Crypto
+from neocore.UInt160 import UInt160
+
+from config import setting
+from data_model.model import Tx, LocalBlockCout, BlockHeight, Token ,logger
+
+
+
+def get_application_log(txid):
+    data = {
+        "jsonrpc": "2.0",
+        "method": "getapplicationlog",
+        "params": [txid],
+        "id": 1
+    }
+
+    try:
+        res = requests.post(random.choice(setting.NEO_RPC_APPLICATION_LOG), json=data).json()
+        if res.get("result"):
+            return res.get("result")
+    except Exception as e:
+        logger.error(e)
+
+
+
+def get_token_info(tokenAddress):
+
+    data = {
+      "jsonrpc": "2.0",
+      "method": "invokefunction",
+      "id": 3
+    }
+
+    token_info = []
+
+    try:
+        for attr in ["name","symbol","decimals"]:
+            data["params"] = [tokenAddress,attr,[]]
+            res = requests.post(random.choice(setting.NEO_RPC_APPLICATION_LOG), json=data).json()
+            value = res.get("result").get("stack")[0].get("value")
+            if res.get("result").get("stack")[0].get("type") == "ByteArray":
+                value = bytearray.fromhex(value).decode()
+                token_info.append(value)
+
+        return token_info
+    except Exception as e:
+        logger.error(e)
+        return None
+
+
+
+class TRANSACTION_TYPE(object):
+    CONTRACT = "ContractTransaction"
+    CLAIM = "ClaimTransaction"
+    INVOKECONTRACT = "InvocationTransaction"
+
+
+localBlockCount = LocalBlockCout.query()
+if localBlockCount:
+
+    local_block_count = localBlockCount.height
+else:
+    local_block_count = 0
+    localBlockCount = LocalBlockCout.save(local_block_count)
+while True:
+    logger.info(local_block_count)
+    block_h = BlockHeight.query()
+    if not block_h:
+        continue
+    if local_block_count <= block_h.height - 1:
+        exist_instance = Tx.query(local_block_count)
+        if exist_instance:
+            for tx in exist_instance:
+                tx_id = tx.tx_id
+                tx_type = tx.tx_type
+
+                # store nep5 tx
+                if tx_type != TRANSACTION_TYPE.INVOKECONTRACT:
+                    continue
+
+                content = get_application_log(tx_id)
+                if not content:
+                    continue
+                if not content.get("notifications"):
+                    continue
+                for notification in content["notifications"]:
+                    contract = notification["contract"]
+                    try:
+                        if bytearray.fromhex(notification["state"]["value"][0]["value"]).decode() != "transfer":
+                            continue
+
+                        if Token.query_token(contract):
+                            continue
+
+                        token_info = get_token_info(contract)
+                        if token_info:
+
+                            Token.save(contract,token_info[0],token_info[1],token_info[2],
+                                       "NEP-5","NEO","https://appserver.trinity.ink/static/icon/{}.png".format(token_info[1]))
+
+                    except Exception as e:
+                        logger.error(e)
+
+        local_block_count += 1
+        localBlockCount.height = local_block_count
+        LocalBlockCout.update(localBlockCount)
+
+
+
+
+    else:
+        time.sleep(10)
+
+
+
+
+
+
