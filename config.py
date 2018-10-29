8ff111924e11b9522089e85508d88879c21b49a6
maiganne <1730105653@qq.com>
2018-10-29 20:45:22 +0800

    modify

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ NEO_RPC_APPLICATION_LOG_POOL=[
     # "http://seed7.ngd.network:10332",
     # "http://seed6.ngd.network:10332",
     # "http://47.254.64.251:10332",
-    # "http://172.20.151.244:10332",
+    "http://172.20.151.244:10332",
     "http://127.0.0.1:10332"
 ]
 
