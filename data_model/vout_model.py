619ce87e99aba6c3a8ff5387a38371f94259312d
maiganne <1730105653@qq.com>
2018-11-07 17:59:39 +0800

    modify

--- data_model/vout_model.py
+++ data_model/vout_model.py
@@ -158,28 +158,6 @@ class Vout(NeoTableBase):
         session.delete(instanse)
 
 
-class HandledTx(NeoTableBase):
-    __tablename__ = 'handled_tx'
-    id = Column(Integer, primary_key=True)
-    tx_id = Column(String(66),unique=True)
-
-
-    @staticmethod
-    def query(session,tx_id):
-        exist_instance = session.query(HandledTx).filter(HandledTx.tx_id==tx_id).first()
-        return exist_instance
-
-    @staticmethod
-    def save(session,tx_id):
-        new_instance = HandledTx(tx_id=tx_id)
-        session.begin(subtransactions=True)
-        try:
-            session.add(new_instance)
-            session.commit()
-        except Exception as e:
-            logger.error(e)
-            session.rollback()
-
 class Vin(NeoTableBase):
     __tablename__ = 'vin'
     id = Column(Integer, primary_key=True)
@@ -204,6 +182,49 @@ class Vin(NeoTableBase):
     def save(session,tx_id,address,asset_id,vout_number,value):
         new_instance = Vin(tx_id=tx_id, address=address, asset_id=asset_id,
                                vout_number=vout_number,value=value)
+        session.begin(subtransactions=True)
+        try:
+            session.add(new_instance)
+            session.commit()
+        except Exception as e:
+            logger.error(e)
+            session.rollback()
+
+class ContractTx(NeoTableBase):
+    __tablename__ = 'contract_tx'
+    id = Column(Integer, primary_key=True)
+    tx_id = Column(String(66))
+    asset = Column(String(66))
+    address_from = Column(String(40),index=True)
+    address_to = Column(String(40),index=True)
+    value = Column(String(30))
+    block_timestamp=Column(Integer)
+    block_height=Column(Integer)
+
+
+
+    @staticmethod
+    def save(session,tx_id,asset,address_from,address_to,value,block_timestamp,block_height):
+        new_instance = ContractTx(tx_id=tx_id,
+                                asset=asset,address_from=address_from,
+                                address_to=address_to,value=value,
+                                block_timestamp=block_timestamp,block_height=block_height)
+        session.add(new_instance)
+
+class HandledTx(NeoTableBase):
+    __tablename__ = 'handled_tx_for_vout'
+    id = Column(Integer, primary_key=True)
+    tx_id = Column(String(66),unique=True)
+
+
+    @staticmethod
+    def query(session,tx_id):
+        exist_instance = session.query(HandledTx).filter(HandledTx.tx_id==tx_id).first()
+        return exist_instance
+
+    @staticmethod
+    def save(session,tx_id):
+        new_instance = HandledTx(tx_id=tx_id)
         session.begin(subtransactions=True)
         try:
             session.add(new_instance)
