112437c0c96e62972c10b1a0a41caa6955355f75
maiganne <1730105653@qq.com>
2019-01-03 15:50:33 +0800

    modify

--- app/TX/interface.py
+++ app/TX/interface.py
@@ -700,8 +700,8 @@ def _check_balance(address,assetId,value):
     balance = 0
     if balances:
         for b in balances:
-            if b.get(assetId):
-                balance = int(b.get(assetId))
+            if b.get("asset") == assetId:
+                balance = int(b.get("value"))
     if balance < int(value):
         return False
     return True
