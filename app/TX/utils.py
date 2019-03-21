bb6dd082ea95a4f1f29b5e2e44c1fcafc91c80c1
maiganne <1730105653@qq.com>
2019-03-21 14:10:28 +0800

    modify

--- app/TX/utils.py
+++ app/TX/utils.py
@@ -80,7 +80,8 @@ def createMultiSigAddress(script):
 
 def create_opdata(address_from, address_to, value, contract_hash):
     op_data = ""
-    value=int(Decimal(str(value)) * pow(10, 8))
+    decimal = 8 if contract_hash != "0xfc732edee1efdf968c23c20a9628eaa5a6ccb934" else 2
+    value=int(Decimal(str(value)) * pow(10, decimal))
     value = binascii.hexlify(BigInteger(value).ToByteArray()).decode()
     scripthash_from = ToAddresstHash(address_from).ToString2()
     scripthash_to = ToAddresstHash(address_to).ToString2()
