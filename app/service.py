441678cf85dd3eca3cb045cc0d75640efa65af04
maiganne <1730105653@qq.com>
2019-03-22 14:40:46 +0800

    modify

--- app/service.py
+++ app/service.py
@@ -390,6 +390,9 @@ def utxo_to_account(tx,target_address,target_asset):
             if not address_mapping_to.get(key):
                 del address_mapping_to[key]
 
+    addressFrom = ""
+    addressTo = ""
+    value = ""
     amount = address_from_asset_info.get(target_asset).get(target_address)
     if amount:
         addressFrom = target_address
