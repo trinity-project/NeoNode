24699226e1556f3d9796769deac03441607854d5
ubuntu <1730105653@qq.com>
2019-05-06 16:26:30 +0800

    modify

--- plugin/redis_client.py
+++
@@ -1,8 +0,0 @@
-import redis
-from config import setting
-
-__all__=["redis_client"]
-
-pool= redis.ConnectionPool(host=setting.REDIS_IP,port=setting.REDIS_PORT,decode_responses=True,password=setting.REDIS_PASSWORD)
-
-redis_client=redis.Redis(connection_pool=pool)
